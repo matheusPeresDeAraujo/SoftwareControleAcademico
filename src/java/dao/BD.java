10f6c21bd6a2320adf1fb131bb90c1957cacadc6
Matheus <mathewskta@gmail.com>
2018-05-14 17:18:11 -0300

    alteracaoSenhaBanco

--- src/java/dao/BD.java
+++ src/java/dao/BD.java
@@ -18,7 +18,7 @@ public class BD {
     public static Connection getConexao() throws SQLException, ClassNotFoundException{
         Connection conexao=null;
         Class.forName("com.mysql.jdbc.Driver");
-        conexao = DriverManager.getConnection("jdbc:mysql://localhost/sca", "root","123");
+        conexao = DriverManager.getConnection("jdbc:mysql://localhost/sca", "root","");
         return conexao;
     }
 }
